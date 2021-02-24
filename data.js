b473de325cc2880cba50707a8db1ab678aed8993
Achal Kumar Rai <achal17akr@gmail.com>
2021-02-24 21:37:59 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,36 @@
+module.exports = function () {
+    return { 
+        products: [
+            { id: 1, name: "Kayak", category: "Watersports", 
+                description: "A boat for one person", price: 275,
+             image: "https://images-na.ssl-images-amazon.com/images/I/41H+V0ybvwL._SX441_BO1,204,203,200_.jpg"},
+            { id: 2, name: "Lifejacket", category: "Watersports", 
+                description: "Protective and fashionable", price: 48.95,
+                image: "https://images-na.ssl-images-amazon.com/images/I/41H+V0ybvwL._SX441_BO1,204,203,200_.jpg" },
+            { id: 3, name: "Soccer Ball", category: "Soccer", 
+                description: "FIFA-approved size and weight", price: 19.50,
+                image: "https://images-na.ssl-images-amazon.com/images/I/41H+V0ybvwL._SX441_BO1,204,203,200_.jpg" },
+            { id: 4, name: "Corner Flags", category: "Soccer", 
+                description: "Give your playing field a professional touch", 
+                price: 34.95,
+                image: "https://images-na.ssl-images-amazon.com/images/I/41H+V0ybvwL._SX441_BO1,204,203,200_.jpg" },
+            { id: 5, name: "Stadium", category: "Soccer", 
+                description: "Flat-packed 35,000-seat stadium", price: 79500,
+                image: "https://images-na.ssl-images-amazon.com/images/I/41H+V0ybvwL._SX441_BO1,204,203,200_.jpg" },
+            { id: 6, name: "Thinking Cap", category: "Chess", 
+                description: "Improve brain efficiency by 75%", price: 16,
+                image: "https://images-na.ssl-images-amazon.com/images/I/41H+V0ybvwL._SX441_BO1,204,203,200_.jpg" },
+            { id: 7, name: "Unsteady Chair", category: "Chess", 
+                description: "Secretly give your opponent a disadvantage", 
+                price: 29.95,
+                image: "https://images-na.ssl-images-amazon.com/images/I/41H+V0ybvwL._SX441_BO1,204,203,200_.jpg" },
+            { id: 8, name: "Human Chess Board", category: "Chess", 
+                description: "A fun game for the family", price: 75,
+                image: "https://images-na.ssl-images-amazon.com/images/I/41H+V0ybvwL._SX441_BO1,204,203,200_.jpg" },
+            { id: 9, name: "Bling Bling King", category: "Chess", 
+                description: "Gold-plated, diamond-studded King", price: 1200,
+                image: "https://images-na.ssl-images-amazon.com/images/I/41H+V0ybvwL._SX441_BO1,204,203,200_.jpg" }
+        ],
+        orders: []
+    }
+}
